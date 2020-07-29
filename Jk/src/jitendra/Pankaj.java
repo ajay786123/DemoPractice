16f4ca6a0d38a135eb2efc3b18421a45af5f3636
Ajay <Ajay@Ajay-PC>
2020-07-29 16:00:12 +0530

    First Commit

---
+++ Jk/src/jitendra/Pankaj.java
@@ -0,0 +1,15 @@
+package jitendra;
+
+public class Pankaj {
+
+	public static void main(String[] args) {
+		int a=10;
+		
+		int b=5;
+		int c;
+		c=a+b;
+	   System.out.println(a+b);
+	   System.out.println(c);
+	}
+   
+   }
